013ad29a97fd2ad372352d64363d1d184f5fc06f
lizziebru <e.bru21@imperial.ac.uk>
2022-01-18 12:17:36 +0000

    Update oaks

--- week7/code/oaks_debugme.py
+++ week7/code/oaks_debugme.py
@@ -1,63 +1,67 @@
 #!/usr/bin/env python3
 
-"""This script writes a function to read data on oaks - the exercise is to debug it"""
+"""Read an input file containing tree names and extract the oak names, saving these to a CSV"""
 
 __author__ = 'Lizzie Bru eab21@ic.ac.uk'
 
 import csv
 import sys
-import doctest
-from difflib import SequenceMatcher
 
-# NOTE: I am taking the instruction of making this script able to 'handle cases where there is a typo etc' to mean: allow for leeway to include names which could be oaks but just have a typo in them
+# NOTE: I am taking the instruction of making this script able to 'handle cases where there is a typo etc' to mean: allow for leeway if the same as quercus except for lowercase or space before
 
 #Define function
 def is_an_oak(name):
-    """ Returns a measure of how similar a name is to 'Quercus' (i.e. how close it is to being an oak)
+    """ Returns true if the tree is an oak (i.e. its name begins with 'quercus')
 
-    >>> is_an_oak('Quercus')
-    1.0
+    >>> is_an_oak('Quercus robur')
+    True
 
-     >>> is_an_oak('Fraxinus')
-     0.4
+    >>> is_an_oak('Pinus sylvestris')
+    False
 
-    whenever there is a typo, returns a value close enough to 1.0
-    >>> is_an_oak('QQuercus')
-    0.9333333333333333
-    >>> is_an_oak('Quercuss')
-    0.9333333333333333
+    allow for lowercase
+    >>> is_an_oak('quercus robur')
+    True
+
+    allow for a space before the start
+    >>> is_an_oak(' Quercus robur')
+    True
+
+    don't allow for other typos
+    >>> is_an_oak('QQuercus robur')
+    False
+    >>> is_an_oak('Quercuss robur')
+    False
     >>> is_an_oak('Quercyuss')
-    0.875
-
-    Let's define 'close enough' as being up to three typos:
-    >>> is_an_oak('uwercuss')
-    0.8
+    False
 
     """
-    return SequenceMatcher(None, name.lower(), "quercus").ratio() # returns the similarity of the input name to 'quercus' as a ratio (.lower makes it all lower-case to standardize comparisons)
-
+    return name.lower().strip().startswith('quercus ')
 
 def main(argv):
-    """Open the input data from TestOaksData.csv and write the outputs (= only the species which have names close enough to being oaks) to JustOaksData.csv"""
+    """Open the input data from TestOaksData.csv and write the outputs (= only the species which have oak names) to JustOaksData.csv"""
+
     f = open('../data/TestOaksData.csv','r') # this csv contains 5 oak species names laid out in 2 columns (genus & species)
     g = open('../data/JustOaksData.csv','w') # this csv is empty
+
     taxa = csv.reader(f)
     csvwrite = csv.writer(g)
-    oaks = set()
+
     for row in taxa:
         print(row)
-        print ("The genus is: ") 
+        print ("The genus is: ")
         print(row[0] + '\n')
-        if is_an_oak(row[0]) == 1.0: # if it's got the exact name 'Quercus'
+        if is_an_oak(" ".join(row)): # if it's an oak, write to JustOaksData.csv
             print('FOUND AN OAK!\n')
             csvwrite.writerow([row[0], row[1]])
-        elif is_an_oak(row[0]) >= 0.8: # if it's 'close enough' to being an oak (defined here as having a similarity of 0.8)
-            print('Close enough to being an oak - but check for typos') # that way it flags the uncertainty and you can manually check these
-    f.close() # need to close the csvs after opening them
+
+    f.close()  # need to close the csvs after opening them
     g.close()
     return 0
     
 if (__name__ == "__main__"):
-    status = main(sys.argv)
+    import doctest
 
-doctest.testmod() # to run with embedded tests
+    doctest.testmod() # to run with embedded tests
+    status = main(sys.argv)
+    sys.exit(status)
